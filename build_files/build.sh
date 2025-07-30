b149bdc0a6113085b98765be6b16a0130fb23d3d
balor <me@balor.pl>
2025-07-30 22:06:29 +0200

    add ivpn

--- build_files/build.sh
+++ build_files/build.sh
@@ -29,3 +29,7 @@ dnf5 -y install libinput-utils systemd-devel libinput-devel ninja-build meson
 # mullvad vpn
 dnf5 -y config-manager addrepo --from-repofile=https://repository.mullvad.net/rpm/beta/mullvad.repo
 dnf5 install mullvad-vpn
+
+# ivpn vpn
+dnf5 -y config-manager addrepo --from-repofile=https://repo.ivpn.net/stable/fedora/generic/ivpn.repo
+dnf5 install ivpn ivpn-ui
